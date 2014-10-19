e0c7d8dcb7f0d0131819a925ca02775d64c2ede1
krim <pasha.rod@mail.ru>
2014-10-19 17:33:30 +0400

    fix proxy type and proxy url in fetch

--- lib/kinopoisk_parser.rb
+++ lib/kinopoisk_parser.rb
@@ -9,11 +9,11 @@ module Kinopoisk
   NotFound   = Class.new StandardError
 
   # Headers are needed to mimic proper request so kinopoisk won't block it
-  def self.fetch(url, proxy = nil, debug = false)
+  def self.fetch(url, proxy_url = nil, proxy_type = nil, debug = false)
     c = Curl::Easy.new(url) do |curl|
-      curl.proxy_url = proxy unless proxy.nil?
+      curl.proxy_url = proxy_url unless proxy_url.nil?
       curl.timeout = 300
-      curl.proxy_type = 5
+      curl.proxy_type = proxy_type unless proxy_type.nil?
       curl.headers['User-Agent'] = 'a'
       curl.headers['Accept-Encoding'] = 'a'
       curl.verbose = debug
@@ -24,10 +24,7 @@ module Kinopoisk
 
   # Returns a nokogiri document or an error if fetch response status is not 200
   def self.parse(url)
-    puts url
     p = fetch url
     p.status.to_i==200 ? Nokogiri::HTML(p.body_str.force_encoding('windows-1251').encode('utf-8')) : raise(NotFound)
   end
 end
-
-
