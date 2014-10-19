5f09b49b25d7e1371e6b116391837f1cadb0f34b
krim <pasha.rod@mail.ru>
2014-10-19 17:31:20 +0400

    use curb + add proxy

--- lib/kinopoisk_parser.rb
+++ lib/kinopoisk_parser.rb
@@ -1,5 +1,5 @@
 require 'nokogiri'
-require 'httpclient'
+require 'curb'
 require 'kinopoisk/movie'
 require 'kinopoisk/search'
 require 'kinopoisk/person'
@@ -9,13 +9,25 @@ module Kinopoisk
   NotFound   = Class.new StandardError
 
   # Headers are needed to mimic proper request so kinopoisk won't block it
-  def self.fetch(url)
-    HTTPClient.new.get url, nil, { 'User-Agent'=>'a', 'Accept-Encoding'=>'a' }
+  def self.fetch(url, proxy = nil, debug = false)
+    c = Curl::Easy.new(url) do |curl|
+      curl.proxy_url = proxy unless proxy.nil?
+      curl.timeout = 300
+      curl.proxy_type = 5
+      curl.headers['User-Agent'] = 'a'
+      curl.headers['Accept-Encoding'] = 'a'
+      curl.verbose = debug
+    end
+    c.perform
+    c
   end
 
   # Returns a nokogiri document or an error if fetch response status is not 200
   def self.parse(url)
+    puts url
     p = fetch url
-    p.status==200 ? Nokogiri::HTML(p.body.encode('utf-8')) : raise(NotFound)
+    p.status.to_i==200 ? Nokogiri::HTML(p.body_str.force_encoding('windows-1251').encode('utf-8')) : raise(NotFound)
   end
 end
+
+
